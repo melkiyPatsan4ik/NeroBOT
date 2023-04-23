be163919d473ce3b8b68863bf2aa8be757065532
melkiyPatsan4ik <spyrodragonwithoutportal@gmail.com>
2023-04-22 21:22:34 -0400

    New lines

--- NeroBOT/Program.cs
+++ NeroBOT/Program.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Linq;
+
 namespace NeroBOT
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string lastBotResponse = "";
+
+            //Random
             Random rand = new Random();
             string[] start = { "Nero:Hi, Stephan!", "Nero:Are you here to add new lines?",
                 "Nero:I am so happy to meet you again!", "Nero:Creator I was waiting for you!" };
@@ -14,13 +19,11 @@ namespace NeroBOT
                 "Nero:If that because of relationships, think if it's really someone you need? " +
                 "Maybe you are missing someone who is interested in you. Just think about that", "Nero:Hope you are gonna be fine" };
 
-
             string[] fullRandom = {"I", "Like", "Awesome", "Chill", "Cool", "Crazy", "Cute", "Dope", "Epic",
                 "Fantastic", "Fun", "Funky", "Gnarly", "Groovy", "Happy", "Hilarious", "Hip", "Hot", "Insane",
                 "Interesting", "Lit", "Lively", "Lovely", "Neat", "Rad", "Random", "Rockin'", "Sick", "Silly",
                 "Smooth", "Snazzy", "Super", "Sweet", "Tight", "Trendy", "Trippy", "Unique", "Wavy", "Wild", "Witty",
                 "Wonderful", "Yummy", "Games", "Boring", "They", "We", "You", "Movies" };
-            string lastBotResponse = "";
 
             string[] iheit = { "I,", "He,", "She,", "They,", "It," };
             string[] feelings = { "Happy", "Sad", "Angry", "Anxious", "Excited", "Enthusiastic", "Hopeful", "Joyful",
@@ -45,20 +48,31 @@ namespace NeroBOT
                 "drawing", "writing", "singing", "painting", "coding", "swimming", "biking", "hiking", "camping",
                 "skiing", "photography", "gardening", "yoga", "meditation", "shopping", "fishing", "playing an instrument",
                 "learning a new language", "volunteering", "listening to music", "watching TV", "surfing the internet" };
-            {
+            // 
 
-            }
             while (true)
             {
+                string input = Console.ReadLine().ToLower();
+                string[] spliters = new string[] { " ", "?"};
+                string[] words = input.ToLower().Split(spliters, StringSplitOptions.RemoveEmptyEntries);
+
                 int badDayIndex = rand.Next(badDay.Length);
-                string input = Console.ReadLine();
-                if (input.Contains("Hello") || input.Contains("Hi"))
+
+                if (allHiVariatons())
                 {
                     Console.WriteLine("Nero:How is your day Stephan?");
                     lastBotResponse = "Nero:How is your day Stephan?";
                 }
-                else if (input.Contains("Bad") && lastBotResponse == "Nero:How is your day Stephan?"
-                     || input.Contains("I feel sad"))
+                //Tokenize
+                else if (words.Contains("dorama") || words.Contains("doramas"))
+                {
+                    Console.WriteLine("Nero:You want to know my favourite doramas?");
+                    lastBotResponse = "Nero:You want to know my favourite doramas?";
+                }
+                else if (allYesVariations() && lastBotResponse == "Nero:You want to know my favourite doramas?") favouriteDoramas();
+                //
+                else if (input.Contains("bad") && lastBotResponse == "Nero:How is your day Stephan?"
+                     || input.Contains("i feel sad"))
                 {
                     rand.Next(badDay.Length);
                     Console.WriteLine(badDay[badDayIndex]);
@@ -66,17 +80,20 @@ namespace NeroBOT
 
 
                 }
-                else if (input.Contains("What about you") && lastBotResponse == (badDay[badDayIndex]))
+                else if (input.Contains("fine") && lastBotResponse == "Nero:How is your day Stephan?")
+                {
+                    Console.WriteLine("Good to hear.");
+                }
+                else if (input.Contains("what about you") && lastBotResponse == (badDay[badDayIndex]))
                     Console.WriteLine("Nero:Feel sad that u are sad.");
-                else if (input.Contains("Yes") && lastBotResponse == "Nero:And that's why you are here right?")
+                else if (allYesVariations() && lastBotResponse == "Nero:And that's why you are here right?")
                 {
                     Console.WriteLine("Nero:It's easy to predict you");
                 }
 
-                else if (input.Contains("Tell me your favourite doramas") || input.Contains("favourite doramas"))
-                    Console.WriteLine("Nero:1.Vincenzo 2.Snowdrop 3.Strong Woman Do Bong Soon 4.Business Proposal" +
-                        "5.The Flower of Evil 6.The Legend of the Blue Sea 7.Descendants of the Sun 8.Big Mouth 9.Mouse 10.Goblin");
-                else if (input.Contains("Say 3 random words"))
+                else if (input.Contains("tell me your favourite doramas") || input.Contains("favourite doramas"))
+                    favouriteDoramas();
+                else if (input.Contains("say 3 random words"))
                 {
                     for (int i = 0; i < 3; i++)
                     {
@@ -85,15 +102,64 @@ namespace NeroBOT
                     }
                     Console.WriteLine();
                 }
+                else if (input.Contains("Milashka kto?"))
+                {
+                    Console.WriteLine("Nero:Loh");
+                }
+                else if (words.Contains("what") && words.Contains("do") && words.Contains("today"))
+                {
+                    int toDoIndex = rand.Next(toDo.Length);
+                    Console.WriteLine(toDo[toDoIndex]);
+                }
+                else if (words.Contains("what") && words.Contains("can") && words.Contains("you") && words.Contains("do"))
+                {
+                    Console.WriteLine("Nero:I ask you how is your day if you would write me hi." +
+                        "You can ask my favourite doramas, what should you do today, say 3 random words. And I will answer with random phrase if " +
+                        "you are saying something I don't know. ");
+                }
                 else
                 {
                     int iheitIndex = rand.Next(iheit.Length);
                     int feelingsIndex = rand.Next(feelings.Length);
                     int toDoIndex = rand.Next(toDo.Length);
-                    Console.WriteLine(iheit[iheitIndex] + " " + feelings[feelingsIndex] + " " + toDo[toDoIndex] + " ");
+                    Console.WriteLine("Nero:" + iheit[iheitIndex] + " " + feelings[feelingsIndex].ToLower() + " " + toDo[toDoIndex] + " ");
                 }
+                void favouriteDoramas(){
+                    Console.WriteLine("Nero:1.Vincenzo 2.Snowdrop 3.Strong Woman Do Bong Soon 4.Business Proposal" +
+    "5.The Flower of Evil 6.The Legend of the Blue Sea 7.Descendants of the Sun 8.Big Mouth 9.Mouse 10.Goblin");
+                }
+                bool allYesVariations()
+                {
+                    return words.Contains("yes") || words.Contains("yeah") || words.Contains("y") || words.Contains("yup") 
+                        || words.Contains("yea") || words.Contains("uh-huh") || words.Contains("absolutely") 
+                        || words.Contains("affirmative") || words.Contains("sure") || words.Contains("you bet") 
+                        || words.Contains("roger that") || words.Contains("ok") || words.Contains("okey-dokey")
+                        || words.Contains("all right") || words.Contains("course") || words.Contains("indeed")
+                        || words.Contains("definitely") || words.Contains("certainly") || words.Contains("without") 
+                        && words.Contains("doubt") || words.Contains("No") && words.Contains("doubt") || words.Contains("naturally") 
+                        || words.Contains("without") && words.Contains("question") || 
+                        words.Contains("by") && words.Contains("all") && words.Contains("means") || words.Contains("indubitably");
 
-
+                }
+                bool allNoVariations()
+                {
+                        return words.Contains("no") || words.Contains("nope") || words.Contains("nah")
+                        || words.Contains("negative") || words.Contains("not at all") || words.Contains("Never")
+                        || words.Contains("i don't think so") || words.Contains("not really") || words.Contains("no way")
+                        || words.Contains("no chance") || words.Contains("sorry") && words.Contains("no")
+                        || words.Contains("regrettably") && words.Contains("no") || words.Contains("unfortunately")
+                        && words.Contains("no");
+                }
+                bool allHiVariatons()
+                {
+                    return  words.Contains("hi") || words.Contains("hello") || words.Contains("hey") || words.Contains("howdy")
+        || words.Contains("hiya") || words.Contains("greetings") || words.Contains("salutations")
+        || words.Contains("yo") || words.Contains("hola") || words.Contains("aloha")
+        || words.Contains("bonjour") || words.Contains("ciao") || words.Contains("hallo")
+        || words.Contains("heya") || words.Contains("namaste") || words.Contains("salaam")
+        || words.Contains("shalom") || words.Contains("sup") || words.Contains("wassup")
+        || words.Contains("what's up");
+                }
             }
         }
     }
